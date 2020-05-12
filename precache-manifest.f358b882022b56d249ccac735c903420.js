ac73fe89d039d619eff8f5a8e5fcc8273fc00f01
Steven Long <steve18gt@gmail.com>
2020-05-11 22:23:33 -0400

    Updates

---
+++ precache-manifest.f358b882022b56d249ccac735c903420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b963090da92ea2ba4cba6f4942cffd5b",
+    "url": "/Image-Gallery/index.html"
+  },
+  {
+    "revision": "31081838833023bcbeaa",
+    "url": "/Image-Gallery/static/css/main.35e90857.chunk.css"
+  },
+  {
+    "revision": "e3432fbede1e9a69ec3e",
+    "url": "/Image-Gallery/static/js/2.16c02a56.chunk.js"
+  },
+  {
+    "revision": "31081838833023bcbeaa",
+    "url": "/Image-Gallery/static/js/main.e4dbd7a6.chunk.js"
+  },
+  {
+    "revision": "d5d76b490519e69e4cd7",
+    "url": "/Image-Gallery/static/js/runtime~main.d0ef4297.js"
+  }
+]);
